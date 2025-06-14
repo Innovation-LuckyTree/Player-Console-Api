62498236c3c752f233f877c354b64edfba17ce80
richard cabatana <rcdev72024@outlook.com>
2025-06-14 22:00:45 +0800

    integration for users

---
+++ src/Player-Console.Infrastructure/Core/Models/Requests/Accounts/BasicUserRegistration.cs
@@ -0,0 +1,11 @@
+﻿namespace HP_Player_Console.Infrastructure.Core.Models.Requests.Accounts
+{
+    public class BasicUserRegistration
+    {
+        public string UserName { get; set; }
+        public string MobileNumber { get; set; }
+        public string? ReferralCode { get; set; }
+        public string Password { get; set; }
+        public string FullName { get; set; }
+    }
+}
